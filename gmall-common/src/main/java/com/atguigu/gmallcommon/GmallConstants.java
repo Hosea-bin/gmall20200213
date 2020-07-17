31d9e0633df69f8935f292dbe57c6392e713b34c
port <port.atguigu.com>
2020-07-17 15:11:54 +0800

    first task

---
+++ gmall-common/src/main/java/com/atguigu/gmallcommon/GmallConstants.java
@@ -0,0 +1,9 @@
+package com.atguigu.gmallcommon;
+
+public class GmallConstants {
+    //启动日志主题
+    public static final String GMALL_START = "REAL_TIME_GMALL_START";
+
+    //事件日志主题
+    public static final String GMALL_EVENT = "REAL_TIME_GMALL_EVENT";
+}
